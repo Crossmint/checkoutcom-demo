5cb3548275c227be233d1b8ec59d747ba849928d
VeniaminC <veniamin@paella.dev>
2025-03-17 11:16:23 -0400

    add headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: `
+							default-src 'self';
+							connect-src 'self' https://*.checkout.com;
+							frame-src 'self' https://*.checkout.com;
+							script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.checkout.com;
+							img-src 'self' https://*.checkout.com;
+							style-src 'self' 'unsafe-inline';
+						`
+							.replace(/\s+/g, " ")
+							.trim(),
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
